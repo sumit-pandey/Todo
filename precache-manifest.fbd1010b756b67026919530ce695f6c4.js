45584fa442d2bd320690e8f357bd6ef975ef1782
Sumit Pandey <sumit_pandey@outlook.com>
2019-08-11 03:02:22 +0530

    initial commit

---
+++ precache-manifest.fbd1010b756b67026919530ce695f6c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e9d04244fcc1346fd21d67538680949",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a72598c9ea34b7cfd2e8",
+    "url": "/static/css/main.86ad3fce.chunk.css"
+  },
+  {
+    "revision": "565a655b23b2284d7016",
+    "url": "/static/js/2.6c306069.chunk.js"
+  },
+  {
+    "revision": "a72598c9ea34b7cfd2e8",
+    "url": "/static/js/main.94902305.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.21333a3ebf06e1833e9524c31ef0e7a7.js"
+  "/precache-manifest.fbd1010b756b67026919530ce695f6c4.js"
 );
 
 self.addEventListener('message', (event) => {
