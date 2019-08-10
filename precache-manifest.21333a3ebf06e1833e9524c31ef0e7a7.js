e4d8a223af44ac489841fb4c90fbebb60f42c0da
Sumit Pandey <sumit_pandey@outlook.com>
2019-08-11 02:26:55 +0530

    initial commit

---
+++ precache-manifest.21333a3ebf06e1833e9524c31ef0e7a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94165bda54dec498114ae879aaf06357",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fab68825d9e61c7cc633",
+    "url": "/static/css/main.86ad3fce.chunk.css"
+  },
+  {
+    "revision": "565a655b23b2284d7016",
+    "url": "/static/js/2.6c306069.chunk.js"
+  },
+  {
+    "revision": "fab68825d9e61c7cc633",
+    "url": "/static/js/main.62393bc6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
